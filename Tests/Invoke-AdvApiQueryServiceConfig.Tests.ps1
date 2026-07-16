b3b63692626d70e38dfab7559dfbf81cd26d8e24
Aaron Jensen <ajensen@webmd.net>
2026-07-15 17:43:17 -0700

    Skipping flaky service that exists on build server.

--- Tests/Invoke-AdvApiQueryServiceConfig.Tests.ps1
+++ Tests/Invoke-AdvApiQueryServiceConfig.Tests.ps1
@@ -59,7 +59,7 @@ Describe 'Invoke-AdvApiQueryServiceConfig' {
         $info.DisplayName | Should -Be 'Windows Time'
     }
 
-    $svcNames = Get-Service | Select-Object -ExpandProperty 'Name'
+    $svcNames = Get-Service | Select-Object -ExpandProperty 'Name' | Where-Object { $_ -notlike 'CDPUserSvc*' }
     It 'queries <_> service' -ForEach $svcNames {
         $svc = Get-Service -Name $_
         $svcHandle =
